3c21e0afc63082b977bedf6238a7d31ae6a63545
Ailun Shen <newhoopsdynasty@gmail.com>
2023-04-15 07:04:48 -0400

    Move dummy data

--- src/EventList.js
+++ src/EventList.js
@@ -1,42 +1,9 @@
 import React, {useState, useEffect } from 'react';
 import {Event} from './Event';
+import events from "./TestData.json";
 import './Main.css';
 
 export const EventList = () => {
-    const events = [
-        {
-          id: 1,
-          name: "Event 1",
-          desc: "Description for event 1",
-          dateTime: "2023-04-05T09:00:00",
-          address: "123 Main St, Anytown, USA",
-          link: "https://example.com/event1"
-        },
-        {
-          id: 2,
-          name: "Event 2",
-          desc: "Description for event 2",
-          dateTime: "2023-04-06T13:00:00",
-          address: "456 Oak St, Anytown, USA",
-          link: "https://example.com/event2"
-        },
-        {
-          id: 3,
-          name: "Event 3",
-          desc: "Description for event 3",
-          dateTime: "2023-04-07T18:30:00",
-          address: "789 Maple St, Anytown, USA",
-          link: "https://example.com/event3"
-        },
-        {
-            id: 4,
-            name: "Top G Party",
-            desc: "Party in Dubai address TBD",
-            dateTime: "2023-04-07T18:30:00",
-            address: "67674 Maple St, Anytown, USA",
-            link: "https://example.com/event3"
-          }
-      ];
 
       const [filterText, setFilterText] = useState("");
       const [filterDate, setFilterDate] = useState(new Date().toLocaleDateString());
